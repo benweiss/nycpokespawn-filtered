ca41731b6e2d8d679f6ef6af73a601c697107277
Ben Weiss <benweiss@bens-mbp.home>
2017-07-16 17:09:19 -0700

    Fix tests

--- scripts/run-on-test-data.js
+++ scripts/run-on-test-data.js
@@ -7,19 +7,21 @@ const getConfig = require("../lib/config.js");
 const fakeTweets1 = require("../test/fixtures/tweets1.json");
 const fakeTweets2 = require("../test/fixtures/tweets2.json");
 
-const stream = new EventEmitter();
+const stream1 = new EventEmitter();
+const stream2 = new EventEmitter();
 const config = getConfig();
 const logger = createLogger(console);
-unownbotFiltered(stream, logger, config);
+unownbotFiltered(stream1, stream2, logger, config);
 
-stream.emit("connected");
+stream1.emit("connected");
+stream2.emit("connected");
 
 let delay = 0;
 for (const tweet1 of fakeTweets1) {
-  setTimeout(() => stream.emit("tweet", tweet1), delay);
+  setTimeout(() => stream1.emit("tweet", tweet1), delay);
   delay += 1000;
 }
 for (const tweet2 of fakeTweets2) {
-    setTimeout(() => stream.emit("tweet", tweet2), delay);
+    setTimeout(() => stream2.emit("tweet", tweet2), delay);
     delay += 1000;
 }
